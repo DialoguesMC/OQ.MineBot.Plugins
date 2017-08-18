1cbe6ec37b393b7673e9fb47f0b2e7737ab20c32
Ignas Sadonis <ignassadonis@gmail.com>
2017-08-18 18:52:28 +0200

    Incremented version

--- GreeterPlugin/PluginCore.cs
+++ GreeterPlugin/PluginCore.cs
@@ -51,7 +51,7 @@ namespace GreeterPlugin
         /// <returns></returns>
         public string GetVersion()
         {
-            return "1.00.00";
+            return "1.01.00";
         }
 
         /// <summary>
