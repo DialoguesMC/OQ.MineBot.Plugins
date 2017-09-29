241c5621c9d4963db9a09830a497929225ad30a3
Ignas Sadonis <ignassadonis@gmail.com>
2017-09-30 00:02:06 +0100

    PathSetting instead of a textbox.

--- TextSpammerPlugin/PluginCore.cs
+++ TextSpammerPlugin/PluginCore.cs
@@ -52,7 +52,7 @@ namespace TextSpammerPlugin
         /// </summary>
         public IPluginSetting[] Setting { get; set; } =
         {
-            new StringSetting("Text file path", "Picks lines from the selected file to spam.", ""),
+            new PathSetting("Text file path", "Picks lines from the selected file to spam.", ""),
             new NumberSetting("Min delay", "", 0, 1000, 60*60*60),
             new NumberSetting("Max delay", "(-1 to always use 'Min delay')", -1, -1, 60*60*60),
             new BoolSetting("Anti-spam", "Should random numbers be added at the end?", false),
