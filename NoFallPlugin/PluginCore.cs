e50cda1b2d413e7c3d33c672b7c2fcd920b5d2d4
Ignas Sadonis <ignassadonis@gmail.com>
2018-03-24 18:44:35 +0000

    Plugin init

---
+++ NoFallPlugin/PluginCore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoFallPlugin.Tasks;
+using OQ.MineBot.PluginBase.Base;
+using OQ.MineBot.PluginBase.Base.Plugin;
+using OQ.MineBot.PluginBase.Bot;
+using OQ.MineBot.PluginBase.Utility;
+
+namespace NoFallPlugin
+{
+    [Plugin(1, "No fall", "Allows the bot to float in air without getting kicked (disables all movement)")]
+    public class PluginCore : IStartPlugin
+    {
+        public override void OnLoad(int version, int subversion, int buildversion) { }
+        public override PluginResponse OnEnable(IBotSettings botSettings)
+        {
+            DiscordHelper.Error("The plugin \"No fall\" disables all movements.", 8518);
+            return new PluginResponse(true);
+        }
+        public override void OnStart()
+        {
+            RegisterTask(new CancelUpdate());
+        }
+    }
+}
---
+++ NoFallPlugin/Tasks/CancelUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OQ.MineBot.PluginBase.Base.Plugin.Tasks;
+using OQ.MineBot.PluginBase.Classes;
+
+namespace NoFallPlugin.Tasks
+{
+    public class CancelUpdate : ITask
+    {
+        public override bool Exec() {
+            return true;
+        }
+
+        public override void Start() { player.events.onPlayerUpdate += EventsOnOnPlayerUpdate; }
+        public override void Stop() { player.events.onPlayerUpdate -= EventsOnOnPlayerUpdate; }
+        private void EventsOnOnPlayerUpdate(IStopToken cancel) { cancel.Stop(); }
+    }
+}
